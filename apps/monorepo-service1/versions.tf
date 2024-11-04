0f59f83581e62f8e4bc58b4880b9f570be998b74
TychStack <tyrone@opslevel.com>
2024-11-04 10:58:15 -0500

    Create versions.tf

---
+++ apps/monorepo-service1/versions.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_version = "~>1.9.1"
+  backend "http" {
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0,  < 6.0.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.0, < 6.0.0"
+    }
+    opsgenie = {
+      source  = "opsgenie/opsgenie"
+      version = ">= 0.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+    onepassword = {
+      source  = "1Password/onepassword"
+      version = "2.1.2"
+    }
+  }
+}
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+provider "google-beta" {
+  project               = var.project_id
+  region                = var.region
+  user_project_override = true
+}
+provider "opsgenie" {
+  api_key = data.onepassword_item.opsgenie_api_key.password
+  api_url = "api.eu.opsgenie.com" #default is api.opsgenie.com
+}
+provider "onepassword" {
+  op_cli_path = var.onepassword_cli_path
+}
